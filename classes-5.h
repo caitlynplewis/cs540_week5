4d49473d16949a1b6de3411168d7431e9db6c1e2
Caitlyn Lewis <cookcai@oregonstate.edu>
2024-05-06 20:36:24 -0700

    Complete assignment

--- classes-5.h
+++ classes-5.h
@@ -1,5 +1,8 @@
 /*** This is just a Skeleton/Starter Code for the External Storage Assignment. This is by no means absolute, in terms of assignment approach/ used functions, etc. ***/
 /*** You may modify any part of the code, as long as you stick to the assignments requirements we do not have any issue ***/
+/*
+Caitlyn Lewis    # 932829852 cookcai@oregonstate.edu
+*/
 
 // Include necessary standard library headers
 #include <string>
@@ -22,6 +25,9 @@ public:
         manager_id = stoi(fields[3]);
     }
 
+    Record(int id, int man_id, string name, string bio)
+    : id(id), manager_id(man_id), name(name), bio(bio){}
+
     //You may use this for debugging / showing the record to standard output. 
     void print() {
         cout << "\tID: " << id << "\n";
@@ -31,7 +37,7 @@ public:
     }
 
     int get_size(){
-        return int(sizeof(int) * 2 + bio.length() + name.length());
+        return int(sizeof(int) * 4 + bio.length() + name.length());
     }
      // Serialize the record for writing to file
     string serialize() const {
@@ -55,29 +61,27 @@ public:
                                              // The starting position i refers to the location of record i in the records and size of that particular record i. 
                                              // This information is crucial, you can load record using this if you know if your record is starting from, e.g., 
                                             // byte 128 and has size 70, you read all characters from [128 to 128 + 70]
+    int num_records = 0;
+    int OFFSET_RESERVED = sizeof(int) * 2 * 10;
     
-    int cur_size = 0; // holds the current size of the 
+    int cur_size = OFFSET_RESERVED; // holds the current size of the page
 
     // Write a Record into your page
     bool insert_record_into_page(Record r){
-        
         // Take a look at Figure 9.9 and read the Section 9.7.2 [Record Organization for Variable Length Records]
         // You may adopt any of the approaches mentioned their. E.g., id $ name $ bio $ manager_id $ separating records with a delimiter / the alternative approaches
         if(cur_size + r.get_size() >= 4096){     // Check the current size of your page. Your page has 4KB memory for storing the records and the slot directory information.   
-
             //You cannot insert the current record into this page
             return false;
         }
         else{
             records.push_back(r);
-            cur_size += r.get_size(); 
             // update slot directory information
-            // slot_directory.push_back(make_pair(offset, serialized.size()));
-
+            slot_directory.push_back(make_pair(cur_size, r.get_size()));
+            cur_size += r.get_size(); 
+            num_records++;
             return true;
         }
-        
-
     }
 
     // Function to write the page to a binary output stream, i.e., EmployeeRelation.dat file
@@ -93,7 +97,24 @@ public:
         //  sizeof(int) bytes to parse these numbers. 
         // After those x bytes, you start storing your records. 
         // You can definitely use $ (delimiter) while storing the slot directory informations /(or,) as you know that these are integers(sizeof(int)) you can read 
+
+        memcpy(page_data + offset, &num_records, sizeof(num_records));
+        offset += sizeof(int);
+
+        for (const auto& slot : slot_directory) {
+            // cout << "Recording pair " << slot.first << ", " << slot.second << endl; 
+            memcpy(page_data + offset, &slot.first, sizeof(slot.first));
+            offset += sizeof(int);
+            memcpy(page_data + offset, &slot.second, sizeof(slot.second));
+            offset += sizeof(int);
+            // insert the slot directory information into the page_data
+            if(offset >= OFFSET_RESERVED) {
+                cout << "ERROR overflowing " << OFFSET_RESERVED << " allocated bytes for slot directory" << endl;
+                return;
+            }
+        }
         
+        offset = OFFSET_RESERVED;
         for (const auto& record : records) {
             string serialized = record.serialize();
 
@@ -103,9 +124,6 @@ public:
         }
         //the above loop just read the id, name, bio etc. You'll also need to store the slot-directory information. So that you can use the slot-directory
         // to retrieve a record.
-        for (const auto& slots : slot_directory) {
-            // insert the slot directory information into the page_data
-        }
 
         // Write the page_data to the EmployeeRelation.dat file 
         out.write(page_data, sizeof(page_data)); // Always write exactly 4KB
@@ -121,10 +139,62 @@ public:
         streamsize bytes_read = in.gcount();
         // You may populate the records and slot_directory from the 4 KB data you just read.
         if (bytes_read == 4096) {
-            
             // Process data to fill the slot directory and the records to handle it according to the structure
             // Assuming slot directory is processed here or elsewhere depending on your serialization method
-                        
+            int cursor = 0;
+            // Process slot directory into this page
+            int num;
+            memcpy(reinterpret_cast<char*>(&num), page_data + cursor, sizeof(num));
+            cursor += sizeof(int);
+
+            for (int i = 0; i < num; i++) {
+                int first, second;
+                memcpy(reinterpret_cast<char*>(&first), page_data + cursor, sizeof(first));
+                cursor += sizeof(int);
+                memcpy(reinterpret_cast<char*>(&second), page_data + cursor, sizeof(first));
+                cursor += sizeof(int);
+                // cout << "Made pair " << first << ", " << second << endl;
+                slot_directory.push_back(make_pair(first, second));
+            }
+            cursor = OFFSET_RESERVED;
+            // Process records into this page
+            for (int i = 0; i < num; i++) {
+                // int - id
+                int id, man_id;
+                memcpy(reinterpret_cast<char*>(&id), page_data + cursor, sizeof(id));
+                cursor += sizeof(id);
+
+                // int - manager id
+                memcpy(reinterpret_cast<char*>(&man_id), page_data + cursor, sizeof(man_id));
+                cursor += sizeof(man_id);
+                
+                // int - size of name
+                int name_size;
+                memcpy(reinterpret_cast<char*>(&name_size), page_data + cursor, sizeof(name_size));
+                cursor += sizeof(name_size);
+
+                // str - name
+                char name[name_size + 1] = {0};
+                memcpy(reinterpret_cast<char*>(&name), page_data + cursor, name_size);
+                cursor += name_size;
+
+                // int - size of bio
+                int bio_size;
+                memcpy(reinterpret_cast<char*>(&bio_size), page_data + cursor, sizeof(bio_size));
+                cursor += sizeof(bio_size);
+
+                // str - bio
+                char bio[bio_size + 1] = {0};
+                memcpy(reinterpret_cast<char*>(&bio), page_data + cursor, bio_size);
+                cursor += bio_size;
+
+                Record r(id, man_id, name, bio);
+                // r.print();
+                // cout << "Cursor is at " << cursor << endl;
+                records.push_back(r);
+                // insert_record_into_page(r);
+            }
+            num_records = slot_directory.size();
             return true;
         }
 
@@ -134,6 +204,7 @@ public:
 
         return false;
     }
+
 };
 
 class StorageManager {
@@ -178,59 +249,62 @@ public:
             while (getline(ss, item, ',')) {
                 fields.push_back(item);
             }
-            //create A record object            
             Record r = Record(fields);
-            // r.print();
-            /***TO_DO***/ 
-
-
-            // Now we will insert that record information to page, i.e., buffer[page_number].
-            // If the page has < 4KB data
-                // We Insert the record into current page in the buffer
-            // else:
-                // go to the next page [page_number++]
-                // Remember You only have 3 pages.
 
             if (!buffer[page_number].insert_record_into_page(r)) {
-
                 // If the current page is full, move to the next page
                 page_number++;
  
                 // If you have used all your 3 pages and they are filled up with records you may use write_into_data_file() to write the pages into the data file
                 // It is like dumping all the information in you pages (Which are in your main memory) into the .dat file (which is in External Storage)
                 if (page_number >= buffer.size()) {
+                    // cout << "Pages are full, dumping to file" << endl;
                     for (page& p : buffer) {
                         p.write_into_data_file(data_file);
                     }
                     // Reset/Free the pages and start filling them up with the future records
                     page_number = 0;  
-                    // page.clear();
+                    buffer.clear();
+                    buffer.resize(3); // You can have maximum of 3 Pages.
                 }
                 // Insert the record into the new current page
                 buffer[page_number].insert_record_into_page(r);
             }
-           
-            
         }
         csvFile.close();  // Close the CSV file
     }
 
     // Searches for an Employee by ID in the binary data_file using the page and prints if found
     void findAndPrintEmployee(int searchId) {
-        
+        data_file.clear();
         data_file.seekg(0, ios::beg);  // Rewind the data_file to the beginning for reading
 
         /*** TO_DO ***/
         // Use [ read_from_data_file(data_file)] to read a page from your datafile to the page buffer[3];
         // You can read into these 3 pages at once or use the following loop to read pages one by one. 
-        int page_number = 0;
-        while(buffer[page_number].read_from_data_file(data_file)){
-
-            // Now that you have a page loaded with the data you stored previously, use the slot directory to find the desired id.
-            // If you have not found the id in these 1/3 pages, repeat the process: reset the pages[3] and load next 3 pages from the EmployeeRelation.dat file   
-
+        while(data_file.good()){
+            int page_number = 0;
+            while(buffer[page_number].read_from_data_file(data_file)){
+                // Now that you have a page loaded with the data you stored previously, use the slot directory to find the desired id.
+                // If you have not found the id in these 1/3 pages, repeat the process: reset the pages[3] and load next 3 pages from the EmployeeRelation.dat file   
+                for (auto& r: buffer[page_number].records) {
+                    // cout << "\tSearching ID " << r.id << endl;
+                    if(r.id == searchId){
+                        cout << "Found match: " << endl;
+                        r.print();
+                        return;
+                    }
+                }
+                // cout << "Advancing page " << page_number << ", file pos " << data_file.tellg() << endl;
+                page_number++;
+                if (page_number >= 3){
+                    break;
+                }
+                buffer.clear();
+                buffer.resize(3);
+            }
         }
-      
         // Print not found message if no match from any of the records
+        cout << "ID " << searchId << " not found." << endl;
     }
 };
--- main-4.cpp
+++ main-4.cpp
@@ -1,6 +1,9 @@
 /*
 Skeleton code for External storage management
 */
+/*
+Caitlyn Lewis    # 932829852 cookcai@oregonstate.edu
+*/
 
 #include <string>
 #include <ios>
@@ -12,7 +15,7 @@ Skeleton code for External storage management
 #include <sstream>
 #include <stdexcept>
 #include <cmath>
-#include "classes.h"
+#include "classes-5.h"
 using namespace std;
 
 
@@ -23,10 +26,19 @@ int main(int argc, char* const argv[]) {
 
     // Assuming the Employee.CSV file is in the same directory, 
     // we want to read from the Employee.csv and write into the new data_file
-    manager.createFromFile("Employee.csv");
+    manager.createFromFile("Employee-3.csv");
 
     // Searching for Employee IDs Using [manager.findAndPrintEmployee(id)]
     /***TO_DO***/ 
-    manager.findAndPrintEmployee(123131);
+    int targetID = -1;
+    while(true) {
+        cout << "Enter an employee ID to find, or 0 to quit: ";
+        cin >> targetID;
+        if(targetID == 0){
+            break;
+        }
+        manager.findAndPrintEmployee(targetID);
+    }
+    
     return 0;
 }
